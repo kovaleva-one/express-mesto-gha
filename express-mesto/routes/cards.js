b79783b2a9ec730db61e30b8fea172d05e0926fc
kovaleva-one <93670169+kovaleva-one@users.noreply.github.com>
2023-04-30 22:23:35 +0300

    Add files via upload

--- express-mesto/routes/cards.js
+++ express-mesto/routes/cards.js
@@ -1,14 +1,36 @@
 const router = require('express').Router();
+// eslint-disable-next-line import/no-unresolved
+const { celebrate, Joi } = require('celebrate');
+
 const {
-  getCards, postCard, deleteCard, putLike, deleteLike, getError,
+  getCards, createCard, deleteCard, likeCard, dislikeCard,
 } = require('../controllers/cards');
 
-router.get('/cards', getCards);
-router.post('/cards', postCard);
-router.delete('/cards/:cardId', deleteCard);
-router.put('/cards/:cardId/likes', putLike);
-router.delete('/cards/:cardId/likes', deleteLike);
+router.get('/', getCards);
 
-router.all('/*', getError);
+router.post('/', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().required().min(2).max(30),
+    link: Joi.string().required().pattern(/^(https?:\/\/(www\.)?)[\w\-._~:/?#[\]@!$&'()*+,;]+\.[\w\-._~:/?#[\]@!$&'()*+,;]+#?$/),
+  }),
+}), createCard);
+
+router.delete('/:cardId', celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().length(24).hex(),
+  }),
+}), deleteCard);
+
+router.put('/:cardId/likes', celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().length(24).hex(),
+  }),
+}), likeCard);
+
+router.delete('/:cardId/likes', celebrate({
+  params: Joi.object().keys({
+    cardId: Joi.string().length(24).hex(),
+  }),
+}), dislikeCard);
 
 module.exports = router;
